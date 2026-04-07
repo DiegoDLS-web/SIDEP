dfd52b1823ecbd9093faa4afa3629d5b60586a67
DiegoDLS-web <di.delasotta@duocuc.cl>
2026-04-07 17:44:46 -0400

    Frontend
    Se arreglan importaciones, se corrigen rutas, se crea vista efectiva de que funciona backend consultando por carros de bomberos

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit {
+  private http = inject(HttpClient);
+  carros: any[] = [];
+
+  ngOnInit() {
+    // Usamos /api gracias al proxy que configuramos en angular.json
+    this.http.get<any[]>('/api/carros').subscribe({
+      next: (data) => this.carros = data,
+      error: (err) => console.error('Error conectando al backend:', err)
+    });
+  }
 }
