409c766eab07e907dd1fe697b491233c218826bf
DiegoDLS-web <di.delasotta@duocuc.cl>
2026-05-20 16:14:25 -0400

    Se actualiza schema prisma con mer oficial, se actualiza directorio SRC

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "SRC/prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "SRC/prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"] as string, // <-- La magia de TypeScript está aquí
